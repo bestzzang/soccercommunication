58b573235d6608732f0d45ad910ed939769b0fd4
bestzzang <54509783+bestzzang@users.noreply.github.com>
2019-12-09 02:28:52 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
 #    'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.twitter',
-    'storages',
     'member.apps.MemberConfig',
 ]
 
